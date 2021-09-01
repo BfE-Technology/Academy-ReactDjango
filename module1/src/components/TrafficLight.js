554a3e27efe48e2cc66f01bdcfce704a6a0187da
Eko Edita <ekoeditaa@gmail.com>
2021-09-02 01:35:09 +0800

    Add check point 4 for module 1
    Signed-off-by: Eko Edita <ekoeditaa@gmail.com>

--- module1/src/components/TrafficLight.js
+++ module1/src/components/TrafficLight.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import TrafficLightContainer from './TrafficLightContainer';
-import Light from './Light';
+import React, { useEffect, useState } from 'react'
+import TrafficLightContainer from './TrafficLightContainer'
+import Light from './Light'
 
-const TRAFFIC_LIGHT_COLORS = ["red", "amber", "green"];
+const TRAFFIC_LIGHT_COLORS = ['red', 'amber', 'green']
 
 function TrafficLight() {
+  const [currentLight, setCurrentLight] = useState(TRAFFIC_LIGHT_COLORS[0])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentLight(currentLight => {
+        const nextIndex = TRAFFIC_LIGHT_COLORS.indexOf(currentLight) + 1;
+        return TRAFFIC_LIGHT_COLORS[nextIndex % 3];
+      })
+    }, 3000)
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
   return (
     <TrafficLightContainer>
-      {TRAFFIC_LIGHT_COLORS.map((color) => <Light color={color} key={color} />)}
+      {TRAFFIC_LIGHT_COLORS.map((color) => (
+        <Light color={color === currentLight ? color : 'gray'} key={color} />
+      ))}
     </TrafficLightContainer>
   )
 }
 
-export default TrafficLight;
+export default TrafficLight
