fb8f21750cec020d7c5b9f0bb48c0f941e5ef4b0
Eko Edita <ekoeditaa@gmail.com>
2021-09-12 16:36:33 +0800

    Module 2 check point 4
    Signed-off-by: Eko Edita <ekoeditaa@gmail.com>

--- module2/traffic/views.py
+++ module2/traffic/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.core import serializers
 
+from .models import TrafficLight
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the traffic index.")
+    latest_traffic_light_list = TrafficLight.objects.all()
+    output = serializers.serialize('json', latest_traffic_light_list)
+    return HttpResponse(output, content_type='application/json')
+    
+
+def detail(request, traffic_light_id):
+    return HttpResponse("You're looking at traffic light %s." % traffic_light_id)
